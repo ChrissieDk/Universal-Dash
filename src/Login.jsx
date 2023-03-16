9aa091d07741047f781fabfc39a71780fd6def24
Christiaan de Kock <christiaandk96@gmail.com>
2023-03-16 08:44:17 +0200

    login ui fixed

--- src/Login.jsx
+++ src/Login.jsx
@@ -7,8 +7,11 @@ import { BsFacebook } from 'react-icons/bs';
 import { BiHide, BiShow } from 'react-icons/bi';
 
 const Login = () => {
+  /* Show hide password logic */
+
   const [showPassword, setShowPassword] = useState(false);
 
+  /* login structure and styling  */
   return (
     <div className='w-full  flex justify-center '>
       <div className='bg-slate-100 flex flex-col items-center justify-center text-center min-h-[60%] min-w-[35%] rounded-2xl border border-slate-300 lg:min-w-[20%]'>
@@ -24,9 +27,10 @@ const Login = () => {
             <label className='mb-1 text-sm' htmlFor="email">Email Address</label>
             <input className='border border-rounded-2xl px-3 py-2 cursor-pointer text-center' for="text" name='email' id='email' placeholder='Enter your email address' />
           </div>
-          <div className="flex flex-col relative">
+          {/* password show hide UI and styling */}
+          <div className="flex flex-col relative w-full">
             <label className='mb-1 text-sm ' htmlFor="email">Password</label>
-            <div className='flex'>
+            <div className='flex relative'>
               <input
                 className='border border-rounded-md px-2 py-2 cursor-pointer text-center flex-grow w-full'
                 type={showPassword ? 'text' : 'password'}
@@ -34,27 +38,27 @@ const Login = () => {
                 id='password'
                 placeholder='Password'
               />
-              {showPassword ? (
-                <BiHide
-                  className='h-full flex items-center'
-                  size={20}
-                  onClick={() => setShowPassword(false)}
-                />
-              ) : (
-                <BiShow
-                  className='h-full flex items-center'
-                  size={20}
-                  onClick={() => setShowPassword(true)}
-                />
-              )}
-              
+              <div className="absolute top-0 bottom-0 flex items-center pointer-events-none z-10 w-full justify-end pr-4">
+                {showPassword ? (
+                  <BiShow
+                    size={20}
+                    onClick={() => setShowPassword(false)}
+                    className="pointer-events-auto right-0"
+                  />
+                ) : (
+                  <BiHide
+                    size={20}
+                    onClick={() => setShowPassword(true)}
+                    className="pointer-events-auto right-0"
+                  />
+                )}
+              </div>
             </div>
-            
           </div>
-          
+
+          {/* Buttons and links */}
 
           <div>
-
             <Link to='/dashboard'><button className='w-full rounded bg-slate-500 text-white py-2 hover:bg-blue-600'>Sign in</button></Link>
           </div>
           <div className="flex justify-between">
@@ -65,6 +69,9 @@ const Login = () => {
             <Link to='/forgotPassword'><div className="text-slate-600 hover:underline cursor-pointer hover:text-blue-600" href="/forgotPassword">Forgot password</div></Link>
           </div>
         </div>
+
+        {/* Alternate login links and icons */}
+
         <div className="relative pb-2 pt-2">
           <div className=" -top-3 left-0 w-full text-center">
             <span className='px-3 text-gray-600'>or continue via</span>
